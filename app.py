6b817bab6b2f1e68ba94071a317fa496e6b11859
dnyanadushing-collab <dnyanadushing@gmail.com>
2026-08-22 10:38:17 +0530

    Add admin login

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask
-from config import Config
-from extensions import db
-
-from routes import auth
-from models import User, Customer, Trip
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db.init_app(app)
-
 with app.app_context():
     db.create_all()
 
-app.register_blueprint(auth)
-print("APP STARTED SUCCESSFULLY")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            password="admin123"
+        )
+        db.session.add(admin)
+        db.session.commit()
