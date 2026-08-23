c1fbc189a30d40661aea7493aa90344d1e02d9c0
dnyanadushing-collab <dnyanadushing@gmail.com>
2026-08-23 09:33:48 +0530

    Add default admin user

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+from flask import Flask
+from config import Config
+from extensions import db
+
+from routes import auth
+from models import User, Customer, Trip
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
 with app.app_context():
     db.create_all()
 
+    # Create default admin user if not already exists
     if not User.query.filter_by(username="admin").first():
         admin = User(
             username="admin",
@@ -8,3 +21,11 @@ with app.app_context():
         )
         db.session.add(admin)
         db.session.commit()
+        print("DEFAULT ADMIN CREATED")
+
+app.register_blueprint(auth)
+
+print("APP STARTED SUCCESSFULLY")
+
+if __name__ == "__main__":
+    app.run(debug=True)
